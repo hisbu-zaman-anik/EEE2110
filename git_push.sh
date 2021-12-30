2cae60baee31179c15ac415ca75f5169b87c6828
hisbu-zaman-anik <hisbuzamananik@gmail.com>
2021-12-30 20:06:54 +0600

    initial commit

---
+++ git_push.sh
@@ -0,0 +1,15 @@
+git add .
+
+echo 'Hello Anik Zaman'
+echo 'Welcome to EEE2110'
+echo 'Enter your commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push 
+git push 
+
+echo 'Your EEE2110 Successfully Updated'
+
+read
